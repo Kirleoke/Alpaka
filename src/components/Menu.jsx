6ac65308e8ff257dc2bd1bb0658160f2eae19138
Kirleoke <shalugin.kirillkpc2442020@gmail.com>
2025-01-02 18:18:06 +0300

    update

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,30 +4,52 @@ import styles from "./../styles/menu.module.scss";
 const Menu = () => {
     const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
     const [activeSubMenu, setActiveSubMenu] = useState(null);
-
-
-    // Переключение бургер-меню
-    const toggleMobileMenu = () => {
-        setMobileMenuOpen(!isMobileMenuOpen);
-    };
-
-    // Открытие подменю для мобильных версий
-    const toggleSubMenu = (menu) => {
-        setActiveSubMenu(activeSubMenu === menu ? null : menu);
-    };
     const [activeSubOffer, setActiveSubOffer] = useState(false);
+    const [submenuTimeout, setSubmenuTimeout] = useState(null);
 
+    const toggleMobileMenu = () => {
+        setMobileMenuOpen((prevState) => !prevState);
+    };
+
+    const handleMouseEnter = (submenu) => {
+        if (submenuTimeout) {
+            clearTimeout(submenuTimeout); // Очищаем таймер, если курсор вернулся
+            setSubmenuTimeout(null);
+        }
+        if (submenu === "offer") {
+            setActiveSubOffer(true);
+        } else {
+            setActiveSubMenu(submenu);
+        }
+    };
+
+    const handleMouseLeave = (submenu) => {
+        const timeout = setTimeout(() => {
+            if (submenu === "offer") {
+                setActiveSubOffer(false);
+            } else {
+                setActiveSubMenu(null);
+            }
+        }, 300); // Задержка в миллисекундах
+        setSubmenuTimeout(timeout);
+    };
 
     return (
         <header className={styles.menu}>
             <div className={styles.menu__container}>
-                <a href="/" className={styles.menu__logo}>h.M</a>
+                <a href="/" className={styles.menu__logo}>
+                    h.M
+                </a>
                 <nav
-                    className={`${styles.menu__nav} ${isMobileMenuOpen ? styles["menu__nav--active"] : ""}`}
+                    className={`${styles.menu__nav} ${
+                        isMobileMenuOpen ? styles["menu__nav--active"] : ""
+                    }`}
                 >
                     <ul className={styles.menu__list}>
                         <li className={styles.menu__item}>
-                            <a href="#" className={styles.menu__link}>Главная</a>
+                            <a href="#" className={styles.menu__link}>
+                                Главная
+                            </a>
                         </li>
 
                         {/* Основное меню */}
@@ -35,15 +57,22 @@ const Menu = () => {
                             className={`${styles.menu__item} ${
                                 activeSubMenu === "main" ? styles["menu__item--active"] : ""
                             }`}
-                            onMouseEnter={() => setActiveSubMenu("main")}
-                            onMouseLeave={() => setActiveSubMenu(null)}
+                            onMouseEnter={() => handleMouseEnter("main")}
+                            onMouseLeave={() => handleMouseLeave("main")}
                         >
                             <a className={styles.menu__link}>
-                                Основное меню
+                                <span>Основное меню</span>
+                                <span className={styles.menu__arrow}></span>
                             </a>
-                            <ul className={styles.menu__submenu}>
+                            <ul
+                                className={`${styles.menu__submenu} ${
+                                    activeSubMenu === "main" ? styles["menu__submenu--visible"] : ""
+                                }`}
+                            >
                                 <li>
-                                    <a href="#" className={styles["menu__submenu-link"]}>Заказать верстку</a>
+                                    <a href="#" className={styles["menu__submenu-link"]}>
+                                        Заказать верстку
+                                    </a>
                                 </li>
                                 <li>
                                     <a href="#" className={styles["menu__submenu-link"]}>
@@ -51,24 +80,33 @@ const Menu = () => {
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="#" className={styles["menu__submenu-link"]}>Хочу работать у вас</a>
+                                    <a href="#" className={styles["menu__submenu-link"]}>
+                                        Хочу работать у вас
+                                    </a>
                                 </li>
                                 <li
                                     className={`${styles["menu__submenu-item"]}`}
-                                    onMouseEnter={() => setActiveSubOffer(true)}
-                                    onMouseLeave={() => setActiveSubOffer(false)}
+                                    onMouseEnter={() => handleMouseEnter("offer")}
+                                    onMouseLeave={() => handleMouseLeave("offer")}
                                 >
-                                    <a href="#" className={styles["menu__submenu-link"]}>Есть предложение</a>
+                                    <a href="#" className={styles["menu__submenu-link"]}>
+                                        <span>Есть предложение</span>
+                                        <span className={styles.menu__arrow__second}></span>
+                                    </a>
                                     <ul
-                                        className={`${styles.menu__submenu} ${activeSubOffer ? styles["menu__submenu--visible"] : ""}`}
-                                        onMouseEnter={() => setActiveSubOffer(true)}
-                                        onMouseLeave={() => setActiveSubOffer(false)}
+                                        className={`${styles.menu__submenu} ${
+                                            activeSubOffer ? styles["menu__submenu--visible"] : ""
+                                        }`}
                                     >
                                         <li>
-                                            <a href="#" className={styles["menu__offer-submenu-link"]}>У меня есть оффер</a>
+                                            <a href="#" className={styles["menu__submenu-link"]}>
+                                                У меня есть оффер
+                                            </a>
                                         </li>
                                         <li>
-                                            <a href="#" className={styles["menu__offer-submenu-link"]}>Сделать партнёром</a>
+                                            <a href="#" className={styles["menu__submenu-link"]}>
+                                                Сделать партнёром
+                                            </a>
                                         </li>
                                     </ul>
                                 </li>
@@ -76,10 +114,14 @@ const Menu = () => {
                         </li>
 
                         <li className={styles.menu__item}>
-                            <a href="#" className={styles.menu__link}>Наши сервисы</a>
+                            <a href="#" className={styles.menu__link}>
+                                Наши сервисы
+                            </a>
                         </li>
                         <li className={styles.menu__item}>
-                            <a href="#" className={styles.menu__link}>Контакты</a>
+                            <a href="#" className={styles.menu__link}>
+                                Контакты
+                            </a>
                         </li>
                     </ul>
                 </nav>
@@ -89,26 +131,18 @@ const Menu = () => {
                     className={styles.menu__burger}
                     onClick={toggleMobileMenu}
                     aria-label="Toggle menu"
-                    style={{
-                        backgroundImage: `url(alpaca-menu/src/assets/burger.svg)`,
-                        // backgroundSize: "cover",
-                    }}
                 ></button>
 
                 {/* Телефон */}
                 <a href="tel:+123456789" className={styles.menu__phone}>
-                    <span
-                        className={styles["menu__phone-icon"]}
-                        style={{
-                            backgroundImage: `url(alpaca-menu/src/assets/Vector.svg)`,
-                            backgroundSize: "contain",
-                        }}
-                    ></span>
+                    <span className={styles["menu__phone-icon"]}></span>
                     <span className={styles["menu__phone-number"]}>+1 (321) 222 - 33 - 33</span>
                 </a>
             </div>
             <div
-                className={`${styles.menu__overlay} ${isMobileMenuOpen ? styles["menu__overlay--active"] : ""}`}
+                className={`${styles.menu__overlay} ${
+                    isMobileMenuOpen ? styles["menu__overlay--active"] : ""
+                }`}
                 onClick={toggleMobileMenu}
             ></div>
         </header>
